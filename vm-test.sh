3d1844680529f9f9a2bec8f1e1eec49800631e53
Hector <hjacales@gmail.com>
2025-08-21 11:02:33 +0200

    chore: qemu files

--- vm-test.sh
+++ vm-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 qemu-system-aarch64 \
-    -M raspi3b \
-    -kernel ./kernel8.img\
-    -dtb bcm2710-rpi-3-b.dtb \
-    -drive if=none,file=/home/hector/Dokument/images/Manjaro-ARM-rpi3-hos-gnome-rpi3-hos-1.00.img,id=hd0,format=raw \
-    -append "console=ttyAMA0 root=/dev/mmcblk0p2 rw rootwait" \
-    -serial stdio \
-    -netdev user,id=mynet -device usb-net,netdev=mynet
+  -M raspi3b \
+  -kernel ./kernel8.img \
+  -dtb ./bcm2835-rpi-b.dtb \
+  -drive file=/home/hector/Dokument/images/Manjaro-ARM-rpi3-hos-gnome-rpi3-hos-1.00.img,format=raw,id=sd,if=none \
+  -device sd-card,drive=sd \
+  -append "console=ttyAMA0,115200 root=/dev/mmcblk0p2 rw rootwait rootfstype=ext4" \
+  -serial stdio \
+  -netdev user,id=mynet -device usb-net,netdev=mynet
